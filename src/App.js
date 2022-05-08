a438775caa73d6873054f9dec73b6d486cf6154a
Praveengupta1 <praveengupta86012@gmail.com>
2022-05-08 22:31:14 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+  const [flag, setFlag] = useState(false);
+  const [file, setFile] = useState(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (name && file) {
+      let data = new FormData();
+      data.append("name", name);
+      data.append("img", file);
+      let options = {
+        method: "post",
+        headers: { "Access-Control-Allow-Origin": "*" },
+        url: "http://localhost:8080/api/upload",
+        data: data,
+      };
+      axios(options)
+        .then((res) => setFlag(res.data.success))
+        .catch((err) => console.log(err));
+    } else {
+      console.log("something went wrong");
+    }
+  };
+  const handleReset = () => {
+    setFlag(false);
+    setFile(null);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo">
+        <img src={require("./logo.png")} alt="logo" />
+        <h3 className="tag_line">Share your file</h3>
+      </div>
+      {!flag && (
+        <div className="form">
+          <form onSubmit={handleSubmit}>
+            <label>Your Name</label>
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label>Attach your file</label>
+
+            <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+            <br />
+            <button onClick={handleSubmit} className="Submit">
+              {" "}
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
+      {flag && (
+        <div style={{ textAlign: "center" }}>
+          <h1 style={{ color: "#ffff" }}>Successfully uploaded </h1>
+          <button onClick={handleReset} className="Submit">
+            Upload More Files
+          </button>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
